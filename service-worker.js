e7268788363434bf2477592014c264cc278f10d2
Ralf Carneborn <ralf.carneborn@gmail.com>
2017-07-27 02:00:23 +0200

    too much..

--- service-worker.js
+++ service-worker.js
@@ -1,50 +1,15 @@
-var CACHE_NAME = 'judgedale-cache-v4';
-var urlsToCache = [
-        './',
-        './index.html',
-        './list',
-        './src/judge-dale-app.html',
-        './images/judgedale.jpg'
-      ];
+/**
+ * @license
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
 
-self.addEventListener('install', function(event) {
-  console.log("[ServiceWorker] Installed");
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+/* eslint no-console: ["error", { allow: ["info"] }] */
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          console.log("[ServiceWorker] found in cache", event.request.url);
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  console.log("[ServiceWorker] Activated");
-  
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+console.info(
+  'Service worker disabled for development, will be generated at build time.'
+);
